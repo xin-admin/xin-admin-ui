1097b60a707bad3038e2b3edf36830dc0e728b0f
liu <2302563948@qq.com>
2026-01-08 00:40:18 +0800

    fix(build): 优化构建配置及依赖管理

--- vite.config.ts
+++ vite.config.ts
@@ -1,64 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { URL, fileURLToPath } from 'node:url';
 
 // https://vite.dev/config/
 export default defineConfig(() => {
   return {
-    build: {
-      rollupOptions: {
-        output: {
-          assetFileNames: chunkInfo => {
-            const name = chunkInfo.names[0];
-
-            if (name?.endsWith('.css')) {
-              return 'css/[name]-[hash].css';
-            }
-
-            const imgExts = ['png', 'jpg', 'jpeg', 'gif', 'svg', 'webp'];
-
-            if (imgExts.some(ext => name?.endsWith(`.${ext}`))) {
-              return 'images/[name]-[hash].[ext]';
-            }
-
-            if (name?.endsWith('.js')) {
-              return 'js/[name]-[hash].js';
-            }
-
-            return 'assets/[name]-[hash].[ext]';
-          },
-          chunkFileNames: chunkInfo => {
-            // 检查文件路径，如果是 pages 目录下的文件，则修改文件名和路径
-            const filePath = chunkInfo.facadeModuleId;
-
-            if (filePath) {
-              // 提取文件的父文件夹作为文件名
-              if (filePath.includes('/src/pages/')) {
-                // 提取文件的父文件夹作为文件名
-                const pageName = filePath.split('/src/pages/')[1];
-                // 替换 [name] 为  name 因为vite不支持
-                const newPath = pageName.replace(/\[([^\]]+)/g, '$1');
-
-                const path = newPath.slice(0, newPath.lastIndexOf('/'));
-
-                return `js/pages/${path}/[name]-[hash].js`;
-              } else if (filePath.includes('/src/components/')) {
-                return `js/components/[name]-[hash].js`;
-              }
-            }
-            return 'js/[name]-[hash].js'; // 默认处理方式
-          },
-        }
-      }
-    },
     plugins: [react(), tailwindcss()],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
-        '~': fileURLToPath(new URL('./', import.meta.url))
-      }
-    },
     server: {
       host: '0.0.0.0',
       port: 3000,
